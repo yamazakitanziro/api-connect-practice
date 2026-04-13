6cf956600b66b2dceac92d37f75a40c9ff5df438
yamazakitanziro <nayongchan7907@gmail.com>
2026-04-14 08:30:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import time
 url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP"
 response = requests.get(url)
-st.write(response.json())
+#st.write(response.json())
 
 for coin in response.json():
     st.write(f"{coin['market']}현재가=\{coin['trade_price']:,}")
