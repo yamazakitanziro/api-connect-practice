49558bc392deac5de255e4a01067e2614d68a7ba
yamazakitanziro <nayongchan7907@gmail.com>
2026-04-14 08:29:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("운동 루틴 추천 앱 🏋️")
+
+import requests
+import time
+url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP"
+response = requests.get(url)
+st.write(response.json())
+
+for coin in response.json():
+    st.write(f"{coin['market']}현재가=\{coin['trade_price']:,}")
