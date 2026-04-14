de8a1f0a5230e610b9f6da102c45d5ad49377d63
yamazakitanziro <nayongchan7907@gmail.com>
2026-04-14 10:44:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import time
 url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP"
 
 #관심코인 선택
-st.write("비트:0,이더:1,리플:2 선택")
-관심코인 = st.button("선택")
-response = requests.get(url)
-관심코인가격=\{response.json()[관심코인]['trade_price']:,}
+관심코인 = st.selectbox("코인 선택", ["BTC", "ETH", "XRP"])
+url2 = f"https://api.upbit.com/v1/ticker?markets=KRW-{관심코인}"
+response2 = requests.get(url2)
+관심코인가격=f"\{response2.json()[0]['trade_price']:,}"
 st.write(f"현재가={관심코인가격}")
