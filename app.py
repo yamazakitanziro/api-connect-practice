8de105b278f7c9ce64fa46be9bfa2b5d139a7313
yamazakitanziro <nayongchan7907@gmail.com>
2026-04-14 11:14:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP"
 url2 = f"https://api.upbit.com/v1/ticker?markets=KRW-{관심코인}"
 response2 = requests.get(url2)
 관심코인가격=f"{response2.json()[0]['trade_price']:,}"
+#문자열이라서 숫자와 비교는 불가 
 st.write(f"현재가={관심코인가격}")
 
 
+#사용자 입력 감시기능
 목표가 = st.text_input("목표가 입력",key="input")
 if 목표가!='':
     현재가=response2.json()[0]['trade_price']
-    if 현재가 <= 목표가:
+    if 현재가 <= int(목표가):
         placeholder.write("BTC 매수 타이밍! 🚨")
     else:
         placeholder.write("탐지중")
