0147cf6d1138553002cca476fb078630cd589fc1
yamazakitanziro <nayongchan7907@gmail.com>
2026-04-14 09:42:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,9 @@ import requests
 import time
 url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-XRP"
 
-#현재가격 확인
+#관심코인 선택
+st.write("비트:0,이더:1,리플:2 선택")
+관심코인 = st.button("선택")
 response = requests.get(url)
-for coin in response.json():
-    print(f"{coin['market']}현재가=\{coin['trade_price']:,}")
-
-#사용자 입력 감시기능
-목표가 = st.text_input("입력하시오",key="input")
-
-while True:
-    response = requests.get(url)
-    현재가=response.json()[0]['trade_price']
-    if 현재가 <= 목표가:
-        placeholder.write("BTC 매수 타이밍! 🚨")
-    else:
-        placeholder.write(f"현재가: {현재가}")
-    time.sleep(5)
+관심코인가격=\{response.json()[관심코인]['trade_price']:,}
+st.write(f"현재가={관심코인가격}")
